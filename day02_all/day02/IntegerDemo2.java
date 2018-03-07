29f7dd8812953a9cfa2b1da5949a881035be4af3
wanlong1234567 <2531165965@qq.com>
2018-03-07 17:26:14 +0800

    关于Integer类型的实例demo

---
+++ day02_all/day02/IntegerDemo2.java
@@ -0,0 +1,29 @@
+package day02;
+/**
+ * 数字类型的包装类提供了两个常量:
+ * MAX_VALUE,MIN_VALUE
+ * 分别表示对应的基本类型的取值范围
+ * @author adminitartor
+ *
+ */
+public class IntegerDemo2 {
+	public static void main(String[] args) {
+		int imax = Integer.MAX_VALUE;
+		System.out.println(imax);
+		
+		int imin = Integer.MIN_VALUE;
+		System.out.println(imin);
+		
+		long lmax = Long.MAX_VALUE;
+		System.out.println(lmax);
+		
+	}
+}
+
+
+
+
+
+
+
+
---
+++ day02_all/day02/IntegerDemo3.java
@@ -0,0 +1,34 @@
+package day02;
+/**
+ * JDK1.5之后推出了一个新的特性:
+ * 自动拆装箱
+ * 该特性是编译器认可，而非虚拟机。该特性是编译器
+ * 在编译源代码时自动补充了基本类型与包装类之间的
+ * 转换代码。从而使程序员不用在为两者之间互转而
+ * 编写转换代码。
+ * 
+ * @author adminitartor
+ *
+ */
+public class IntegerDemo3 {
+	public static void main(String[] args) {
+		/*
+		 * 自动拆箱
+		 * 编译器会补充代码到class文件中:
+		 * int d = new Integer(123).intValue();
+		 */
+		int d = new Integer(123);
+		/*
+		 * 自动装箱
+		 * 编译器会补充代码到class文件中:
+		 * Integer in = Integer.valueOf(d);
+		 */
+		Integer in = d;
+	}
+}
+
+
+
+
+
+
